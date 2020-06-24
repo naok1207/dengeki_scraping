489da360bab5bf331c966d0b6fbd8089a3b1eb11
zenos <zenos.game.anime.all.vo1@gmail.com>
2020-06-25 03:56:53 +0900

    books_controller中身追加+フロント編集

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module Dengeki
     config.i18n.default_locale = :ja
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
     config.generators do |g|
       g.javascripts false
       g.helper false
